d5cdc3e96fa5cde4c1b9966693358b0d37f1fcc0
rauda08 <raudajnnh08@gmail.com>
2023-03-27 09:38:46 +0700

    Add project files.

---
+++ exerciseProsedural_113/exerciseProsedural_113.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a,double b){
+    return (a + b) / 2;
+}
+
+string status(double c) {
+    if (c >= 70)
+        return "Lulus"
+    else
+        return "Gagal"
+}
+
+int main() {
+    if (rerata >= 70 || nil > 80)
+        return "Lulus"
+    else
+        return "Gagal"
+}
+
+int main(){
+    double nilM, nilB;
+    cout << "Masukan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukan nilai Bahasa Inggris = ";
+
+
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
